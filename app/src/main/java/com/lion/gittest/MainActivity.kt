129c7ed0956cceaad63342bad0be2435519f1073
zzangse1 <zzangse1014@gmail.com>
2024-11-07 11:19:33 +0900

    viewBinding Setting

--- app/src/main/java/com/lion/gittest/MainActivity.kt
+++ app/src/main/java/com/lion/gittest/MainActivity.kt
@@ -5,12 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.lion.gittest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var activityMainBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
